c97bf716c30c7db20b01f5d862f2c509f6be8393
Edwin Saa <edwin.saa@udla.edu.ec>
2025-06-25 23:11:05 -0500

    First part wather module implementation: Weather model, weather and geolocatization services

---
+++ Notes/Services/WeatherServices.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Notes.Models;
+
+namespace Notes.Services
+{
+    internal class WeatherServices
+    {
+        public async Task<WeatherData> GetWeatherDataAsync(double latitude, double longitude) 
+        {
+            string latitude_str = latitude.ToString().Replace(',', '.');
+            string longitude_str = longitude.ToString().Replace(',', '.');
+
+            string url = "https://api.open-meteo.com/v1/forecast?latitude="+latitude_str+"&longitude="+longitude_str+"&current=temperature_2m,relative_humidity_2m,rain";
+
+            using (HttpClient client = new HttpClient())
+            {
+                var response = await client.GetAsync(url);
+                response.EnsureSuccessStatusCode();
+                var result = await response.Content.ReadAsStringAsync();
+            }
+            
+        }
+    }
+}
