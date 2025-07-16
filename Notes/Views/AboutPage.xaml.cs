afdb173781b38207b870e57a59c7fdb4ee132bd9
Edwin Saa <edwin.saa@udla.edu.ec>
2025-07-16 11:32:13 -0500

    AboutPage xaml y .cs Improvement, addition of buttons Part2

--- Notes/Views/AboutPage.xaml.cs
+++ Notes/Views/AboutPage.xaml.cs
@@ -7,9 +7,28 @@ public partial class AboutPage : ContentPage
 		InitializeComponent();
 	}
 
-    private async void LearnMore_Clicked(object sender, EventArgs e)
+    private async void Videojuegofavorita_Clicked(object sender, EventArgs e)
     {
-        // Navigate to the specified URL in the system browser.
-        await Launcher.Default.OpenAsync("https://aka.ms/maui");
+        var uri = new Uri("https://deathstranding.fandom.com/wiki/Death_Stranding");
+        await Launcher.Default.OpenAsync(uri);
+    }
+
+    private async void Videojuegofavoritapatt_Clicked(object sender, EventArgs e)
+    {
+        var uri = new Uri("https://discoelysium.fandom.com/wiki/Disco_Elysium_Wiki");
+        await Launcher.Default.OpenAsync(uri);
+    }
+
+    private async void Videojuegofavoritapserg_Clicked(object sender, EventArgs e)
+    {
+        var uri = new Uri("https://megamitensei.fandom.com/wiki/Persona_5");
+        await Launcher.Default.OpenAsync(uri);
+    }
+
+    private async void U_Clicked(object sender, EventArgs e)
+    {
+        var uri = new Uri("https://www.udla.edu.ec/");
+        await Launcher.Default.OpenAsync(uri);
+
     }
 }
